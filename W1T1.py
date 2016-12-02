000fd5095798a37262360a2e37f3611186d11e3d
Loulou95 <louisversace@yahoo.co.uk>
2016-12-02 12:11:22 +0000

    Add files via upload

---
+++ W1T1.py
@@ -0,0 +1,9 @@
+import random
+list = [20, 16, 10, 5];
+x = []
+for i in range (0, len(list), 1):
+    randomnumber = random.randint(0, len(list)-1)
+    minus = list.pop(randomnumber)
+    x.append(minus)
+
+print (x) 
