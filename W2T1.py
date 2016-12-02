968f5902030a3cba82dd7a0a2accb047cb9713c4
Loulou95 <louisversace@yahoo.co.uk>
2016-12-02 13:18:03 +0000

    Add files via upload

---
+++ W2T1.py
@@ -0,0 +1,21 @@
+def highestPerfSquare(n):
+    x = 0
+    squares = []
+    if n > 0:
+        xy = x**2
+        while x < n :
+            xy = x**2
+            x += 1
+            squares.append(xy)
+            print(xy)
+    	
+    elif xy != n:
+        nhps = squares[-2]
+    
+        print ("%s is the next highest perfect square.." % nhps)
+
+    else:
+        print ("%s is a perfect square" % xy) 
+
+n = 26
+highestPerfSquare(n)
